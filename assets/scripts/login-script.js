0ef224432b4522454d103a3a229cf83ecfaf5259
RiskiAbdi Rahmawan <riskiabdi0806@gmail.com>
2025-02-12 20:47:42 +0700

    Menambahkan Komentar pada Kode

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -14,21 +14,23 @@ const inputPasswordElement = document.querySelector('#inputPassword');
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* Comment :  Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
-    event.preventDefault();
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
-    const email = inputEmailElement.value;
-    const password = inputPasswordElement.value;
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+  event.preventDefault();
+  // TODO 1 : Mendapatkan input email dan password pengguna dari form.
+  /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+  const email = inputEmailElement.value;
+  const password = inputPasswordElement.value;
+  /* TODO 2 : Buat Logika perbandingan dengan kondisi:
        Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
        Jika tidak, maka panggil fungsi showPopUp().
      */
-    /* Comment: Memastikan bahwa nilai email dan password sesuai dengan nilai yang diharapkan */
-    if (email === expectedEmail && password === expectedPassword) {
-      /* Comment: Jika sesuai maka program akan berpindah ke halaman home. */
-        goToHome();
-    } else {
-      /* Comment: Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
-      showPopUp();
-      }
+  /* Comment: Memastikan bahwa nilai email dan password sesuai dengan nilai yang diharapkan */
+  if (email === expectedEmail && password === expectedPassword) {
+    /* Comment: Jika sesuai maka program akan berpindah ke halaman home. */
+    goToHome();
+  } else {
+    /* Comment: Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
+    showPopUp();
+  }
 });
